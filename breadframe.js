ee3c119a02d54a65e47779fedb8598560e09fa55
latvia <126933766+bachwebsite@users.noreply.github.com>
2023-12-03 18:52:58 -0600

    Update breadframe.js

--- breadframe.js
+++ breadframe.js
@@ -43,4 +43,6 @@ function breadframe(url, selector) {
    var base64 = btoa(code);
     // Create a data URI scheme that specifies the script type and the base64 string
   var dataURI = "data:text/javascript;base64," + base64;
+
+  document.getElementById('myIframe').src=url;
 }
